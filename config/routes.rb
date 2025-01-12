d5cfa7585c4c700503a178b3382f5297fe7658f9
daisuke-shimura <daisuke.s0707@icloud.com>
2025-01-12 16:00:14 +0900

    admin public の作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :admin, skip: [:registrations, :passwords], controllers:{
+    sessions: "admin/sessions"
+  }
+  devise_for :customers, skip: [:passwords], controllers:{
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
