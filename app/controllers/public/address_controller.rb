d105921296d540560921f5ad3a273776678d011a
EC2 Default User <ec2-user@ip-172-31-9-164.ap-northeast-1.compute.internal>
2025-02-20 05:11:37 +0000

    配送先機能 -1

--- app/controllers/public/address_controller.rb
+++ app/controllers/public/address_controller.rb
@@ -1,2 +1,26 @@
 class Public::AddressController < ApplicationController
+  def index
+    @addresses = Address.all
+    @address = Address.new
+  end
+
+  def show
+    
+  end
+
+  def create
+  end
+
+  def update
+    
+  end
+
+  def destroy
+    
+  end
+
+  private
+  def address_params
+    params.require(:address).permit(:name, :number, :place)
+  end
 end
