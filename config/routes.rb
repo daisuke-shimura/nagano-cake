837e5b617f108e50f4c098ca8d1ff2eca4829f98
EC2 Default User <ec2-user@ip-172-31-9-164.ap-northeast-1.compute.internal>
2025-04-14 05:37:57 +0000

    all_destroyパス編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,23 @@
 Rails.application.routes.draw do
-
+#admin
   devise_for :admin, skip: [:registrations, :passwords], controllers:{
     sessions: "admin/sessions"
   }
+
+  namespace :admin do
+    root to: 'homes#top'
+    resources :items, only: [:index, :show, :new, :edit, :create, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+    resources :customers, only: [:index, :show, :edit, :update]
+    resources :orders, only: [:show, :update]
+    patch 'orders/make/:id' => 'orders#make', as: 'order_make'
+  end
+
+#public
   devise_for :customers, skip: [:passwords], controllers:{
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
-
-  namespace :admin do
-    resources :items
-    root to: 'items#top'
-  end
 
   scope module: :public do
     root to: 'homes#top'
@@ -23,9 +29,10 @@ Rails.application.routes.draw do
     get 'customers/confirm/:id' => 'customers#confirm', as: 'customer_confirm'
     patch 'customers/quit/:id' => 'customers#quit', as: 'customer_quit'
 
-    resources :cart_items, only: [:index, :update, :create, :destroy]
     delete 'cart_items/all_destroy' => 'cart_items#all_destroy'
+    resources :cart_items, only: [:index, :update, :create, :destroy]
 
+    #注文履歴削除（destroy）は要らない
     resources :orders, only: [:index, :new, :show, :create, :destroy]
     get 'orders/confirm/:id' => 'orders#confirm', as: 'order_confirm'
     get 'orders/decision/:id' => 'orders#decision', as: 'order_decision'
