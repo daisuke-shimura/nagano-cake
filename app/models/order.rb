5df5dfcc4407c390ec7e309a5df318283f5abd26
EC2 Default User <ec2-user@ip-172-31-9-164.ap-northeast-1.compute.internal>
2025-04-24 06:13:28 +0000

    enum 記載

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,5 +1,6 @@
 class Order < ApplicationRecord
   enum payment_method: { credit_card: 0, transfer: 1 }
+  enum order_status: { waiting_payment: 0, confirm_payment: 1, making: 2, preparing_ship: 3, shipped: 4}
   belongs_to :customer
   has_many :order_details
 end
--- app/models/order_detail.rb
+++ app/models/order_detail.rb
@@ -1,4 +1,5 @@
 class OrderDetail < ApplicationRecord
+  enum making_status: { not_making: 0, waiting_making: 1, making: 2, making_complete: 3}
   belongs_to :item
   belongs_to :order
 end
