887953334b37fc7b9e9a41b0dc55b80b37396b2f
EC2 Default User <ec2-user@ip-172-31-9-164.ap-northeast-1.compute.internal>
2025-05-01 03:37:52 +0000

    ジャンル検索

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,8 +1,17 @@
 class Public::ItemsController < ApplicationController
   #before_action :authenticate_customer!
   def index
-    @items = @q.result(distinct: true).where(is_active: true).page(params[:page]).per(8)
     @genres = Genre.all
+
+    if params[:genre_id].present?
+      @items = @q.result(distinct: true).where(is_active: true, genre_id: params[:genre_id]).page(params[:page]).per(8)
+      @index_name = Genre.find(params[:genre_id]).name
+      @count = @q.result(distinct: true).where(is_active: true, genre_id: params[:genre_id]).count
+    else
+      @items = @q.result(distinct: true).where(is_active: true).page(params[:page]).per(8)
+      @index_name = "商品"
+      @count = @q.result(distinct: true).where(is_active: true).count
+    end
   end
 
   def show
